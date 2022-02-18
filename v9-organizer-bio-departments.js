8a5fddc1325704682aa57cec6bf96801c6625f4b
Vic Chimenti <chimenti@seattleu.edu>
2022-02-18 14:32:32 -0800

    cm cd

--- v9-organizer-bio-departments.js
+++ v9-organizer-bio-departments.js
@@ -240,8 +240,47 @@
          let beginningHTML = '<article class="departmentBioWrapper card shadow-sm border-0 radius-0 mb-3" id="departmentBio' + departmentBioDict.contentId.content + '" aria-label="' + departmentBioDict.articleTitle.content + '">';
  
  
- 
-  
+
+        /***
+          *  bs5 horizontal card
+          * 
+          * */
+        //  <div class="card mb-3" style="max-width: 540px;">
+        //     <div class="row g-0">
+        //         <div class="col-md-4">
+        //         <img src="..." class="img-fluid rounded-start" alt="...">
+        //         </div>
+        //         <div class="col-md-8">
+        //         <div class="card-body">
+        //             <h5 class="card-title">Card title</h5>
+        //             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        //             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        //         </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+
+
+        /***
+          *  bs5 card image overlay
+          * 
+          * */
+        // <div class="card bg-dark text-white">
+        //     <img src="..." class="card-img" alt="...">
+        //     <div class="card-img-overlay">
+        //         <h5 class="card-title">Card title</h5>
+        //         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        //         <p class="card-text">Last updated 3 mins ago</p>
+        //     </div>
+        // </div>
+
+
+
+
+
+             
          /***
           *  check for fulltext content
           * 
