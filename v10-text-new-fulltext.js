50019b66e567647f88bfb4528fc71778f57b7326
Vic Chimenti <chimenti@seattleu.edu>
2025-02-03 06:44:26 -0800

    udv

--- v10-text-new-fulltext.js
+++ v10-text-new-fulltext.js
@@ -1,6 +1,6 @@
 /**
 * @file v10/text/new-fulltext.js
-* @version 1.2.2
+* @version 1.2.3
 * @fileoverview Faculty and Staff Bio Profile Layout
 * @desc Profile layout for Faculty and Staff Bio content type
 * @author Victor Chimenti
