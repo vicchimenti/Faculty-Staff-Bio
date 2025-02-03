2778d193e7403d43d5aa53f7ac0a9973b3a67bc6
Vic Chimenti <victor.chimenti@gmail.com>
2025-02-02 17:03:47 -0800

    ud photo tag

--- v10-text-new-fulltext.js
+++ v10-text-new-fulltext.js
@@ -143,7 +143,7 @@ function getContentValues(tag) {
  let contentDict = {
     // Profile Info
     photo: getContentValues('<t4 type="content" name="Photo" output="normal" />'),
-    photoAlt: getContentValues('<t4 type="content" name="Photo" output="normal" formatter="text/alt" />'),
+    photoAlt: getContentValues('<t4 type="content" name="Photo" output="normal" formatter="v10/image/pxl-crop" cdn="true" pxl-filter-id="66" />'),
     name: getContentValues('<t4 type="content" name="Name of Faculty or Staff Member" output="normal" modifiers="striptags,htmlentities" />'),
     staffType: getContentValues('<t4 type="content" name="Type of Staff" output="normal" display_field="value" delimiter="|" />'),
     position: getContentValues('<t4 type="content" name="Position Title(s)" output="normal" modifiers="nl2br" />'),
